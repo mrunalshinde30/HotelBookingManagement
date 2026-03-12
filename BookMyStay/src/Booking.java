b5cd2daf9ab65d7e4e1d795723731c8b15c9fadb
mrunalshinde30 <ms4236@srmist.edu.in>
2026-03-12 09:55:50 +0530

    created UC1

---
+++ BookMyStay/src/Booking.java
@@ -0,0 +1,25 @@
+/**
+ * Booking
+ *
+ * Entry point of the Book My Stay Hotel Booking application.
+ * Displays welcome message and application version.
+ *
+ * @author Student
+ * @version 1.0
+ */
+
+public class Booking {
+
+    /**
+     * Main method – starting point of the program
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=====================================");
+
+        System.out.println("Application started successfully.");
+    }
+}
